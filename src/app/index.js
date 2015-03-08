ef6a61b03077c04387f94a1dd03e511ffe5be0db
David Elmer <davelmer@hotmail.com>
2015-03-08 18:53:13 -0400

    Initial scaffolding

---
+++ src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('ngFlickrSearchr', ['ngAnimate', 'ngSanitize', 'ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
---
+++ src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('ngFlickrSearchr')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
